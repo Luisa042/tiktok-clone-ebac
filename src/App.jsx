8e8a5fe5fd9f5fa1967cdfaf97d3f94a611f7c78
Luisa <luisa.santos042@gmail.com>
2023-03-29 20:07:46 -0300

    initial commit
    start react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from './components/Hello/hello';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello/hello.jsx
@@ -0,0 +1,7 @@
+import './hello.css';
+
+function Hello() {
+  return <h1 className='logo'>hello!</h1>;
+}
+
+export default Hello;
